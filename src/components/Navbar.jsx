e29725fde4e9745850817f534aeedcdd79c48b90
Kkknight-12 <mayanks365@gmail.com>
2024-01-17 22:00:16 +0530

    Fix navbar on small screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,116 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { Link, Stack } from '@mui/material';
-import { navigationLink } from '../constants/index.js';
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import IconButton from '@mui/material/IconButton'
+import MenuItem from '@mui/material/MenuItem'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Box from '@mui/material/Box'
+import { Link, Stack } from '@mui/material'
+import { navigationLink } from '../constants/index.js'
 
 function NavbarComponent() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null)
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
   return (
-    <AppBar position='static'>
-      <Container maxWidth='xl'>
+    <AppBar position="static">
+      <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Stack
-            direction='row'
+            direction="row"
             spacing={4}
-            justifyContent='space-between'
-            className='w-full'
+            justifyContent="space-between"
+            className="w-full"
           >
             <Typography
-              variant='h5'
+              variant="h5"
               noWrap
-              component='a'
-              href='#app-bar-with-responsive-menu'
-              className='mr-2 font-mono font-bold tracking-wider text-inherit no-underline'
+              component="a"
+              href="#app-bar-with-responsive-menu"
+              className="mr-2 font-mono font-bold tracking-wider text-inherit no-underline"
             >
               LOGO
             </Typography>
 
-            <Stack direction='row' spacing={1} className='gap-2 self-center '>
+            <Stack
+              sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
+              direction="row"
+              spacing={1}
+              className="gap-2 self-center "
+            >
               {navigationLink.map((page) => (
                 <React.Fragment key={page.id}>
                   <Link
-                    className='!text-white hover:no-underline'
+                    className="!text-white hover:no-underline"
                     href={page.href}
                   >
-                    <p className='text-xs md:text-base lg:text-base'>
+                    <p className="text-xs md:text-base lg:text-base">
                       {page.title}
                     </p>
                   </Link>
                 </React.Fragment>
               ))}
             </Stack>
+
+            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {navigationLink.map((page) => (
+                  <MenuItem
+                    key={page.id}
+                    onClick={handleCloseNavMenu}
+                    className="!py-0"
+                  >
+                    <Link
+                      className="text-base text-blue-500 !no-underline"
+                      href={page.href}
+                    >
+                      {page.title}
+                    </Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
           </Stack>
         </Toolbar>
       </Container>
     </AppBar>
-  );
+  )
 }
-export default NavbarComponent;
+export default NavbarComponent
