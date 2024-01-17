3cb4924cab4be402e68708d90514cc25d2600a4f
Anshu Patel <anshu@techstuff.cloud>
2024-01-17 10:21:56 +0530

    NavBarFix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,42 @@
-import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Container from '@mui/material/Container'
-import { Link, Stack } from '@mui/material'
-import { navigationLink } from '../constants/index.js'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Link, Stack } from '@mui/material';
+import { navigationLink } from '../constants/index.js';
 
 function NavbarComponent() {
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
+    <AppBar position='static'>
+      <Container maxWidth='xl'>
         <Toolbar disableGutters>
           <Stack
-            direction="row"
+            direction='row'
             spacing={4}
-            justifyContent="space-between"
-            className="w-full"
+            justifyContent='space-between'
+            className='w-full'
           >
             <Typography
-              variant="h5"
+              variant='h5'
               noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              className="mr-2 font-mono font-bold tracking-wider text-inherit no-underline"
+              component='a'
+              href='#app-bar-with-responsive-menu'
+              className='mr-2 font-mono font-bold tracking-wider text-inherit no-underline'
             >
               LOGO
             </Typography>
 
-            <Stack direction="row" spacing={1} className="gap-2">
+            <Stack direction='row' spacing={1} className='gap-2 self-center '>
               {navigationLink.map((page) => (
                 <React.Fragment key={page.id}>
                   <Link
-                    className="!text-white hover:no-underline"
+                    className='!text-white hover:no-underline'
                     href={page.href}
                   >
-                    {page.title}
+                    <p className='text-xs md:text-base lg:text-base'>
+                      {page.title}
+                    </p>
                   </Link>
                 </React.Fragment>
               ))}
@@ -43,6 +45,6 @@ function NavbarComponent() {
         </Toolbar>
       </Container>
     </AppBar>
-  )
+  );
 }
-export default NavbarComponent
+export default NavbarComponent;
