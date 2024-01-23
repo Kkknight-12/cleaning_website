551326ac8c4ea8a48948decc8716cec0e0fda021
Kkknight-12 <mayanks365@gmail.com>
2024-01-23 09:00:25 +0530

    Versioning untracked files

--- src/icons/Packaging.jsx
+++ src/icons/Packaging.jsx
@@ -12,11 +12,11 @@ export const Packaging = ({ styles }) => {
       xml:space="preserve"
       className={styles}
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {' '}
