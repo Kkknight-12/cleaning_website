0955bb14301400e5543ad3681a604b8d5faf99e0
Kkknight-12 <mayanks365@gmail.com>
2024-01-26 10:32:47 +0530

    Product page card fix

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -7,18 +7,18 @@ export const ProductsPage = () => {
     <Stack className="items-center mt-20">
       <p className="text-6xl font-bold text-blue-400 mb-10">Products</p>
 
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {productsPageContent.map((product) => {
           return (
             <Stack
               key={product.id}
-              className="items-center flex-wrap border border-gray-200 rounded-2xl p-5"
+              className="items-center flex-wrap border border-gray-200 rounded-2xl p-5 bg-white-100 shadow-xl"
             >
-              <div className="max-w-[320px] mt-8 md:max-w-full !mx-auto">
+              <div className="items-center justify-center flex flex-col max-w-[320px] mt-8 md:max-w-full !mx-auto">
                 <ImageContainer
                   src={product.image_path}
                   alt={product.image_alt}
-                  styles="object-contain rounded-2xl overflow-hidden max-h-[400px]"
+                  styles="object-contain rounded-2xl overflow-hidden max-h-[400px] "
                 />
                 <Stack spacing={2} className="mt-5">
                   <p className="text-center text-2xl font-bold tracking-wide">
